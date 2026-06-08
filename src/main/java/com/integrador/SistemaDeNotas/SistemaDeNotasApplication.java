a30a0f412860531d32423d925b109af7b2b168e2
alisson <alissonfranco56@gmail.com>
2026-06-07 22:14:54 -0500

    mantenimiento

--- src/main/java/com/integrador/SistemaDeNotas/SistemaDeNotasApplication.java
+++ src/main/java/com/integrador/SistemaDeNotas/SistemaDeNotasApplication.java
@@ -17,24 +17,20 @@ public class SistemaDeNotasApplication {
 		SpringApplication.run(SistemaDeNotasApplication.class, args);
 	}
 
-	// Este código se ejecuta automáticamente al iniciar la aplicación
     @Bean
     public CommandLineRunner inicializarDatos(UsuarioRepository usuarioRepo, PasswordEncoder passwordEncoder) {
         return args -> {
-            // Solo creamos el usuario si la base de datos está vacía
             if (usuarioRepo.count() == 0) {
                 Usuario admin = new Usuario();
                 admin.setNombre("Admin");
                 admin.setApellido("Principal");
                 admin.setCorreo("admin@jcm.edu.pe");
                 
-                // Encriptamos la contraseña "Admin123!" (cumple con tus reglas: mayúscula, minúscula, número, especial, +8 letras)
                 admin.setContrasena(passwordEncoder.encode("Admin123!")); 
                 
                 admin.setRol(Usuario.RolUsuario.ADMIN);
                 admin.setEstado(true);
 
-                // Creamos su registro correspondiente en la tabla Administrador
                 Administrador detalleAdmin = new Administrador();
                 detalleAdmin.setUsuario(admin);
                 admin.setAdministrador(detalleAdmin);
--- src/main/java/com/integrador/SistemaDeNotas/repositorio/AlumnoRepository.java
+++ src/main/java/com/integrador/SistemaDeNotas/repositorio/AlumnoRepository.java
@@ -1,5 +1,7 @@
 package com.integrador.SistemaDeNotas.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.integrador.SistemaDeNotas.modelo.entidades.Alumno;
 
 @Repository
 public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
-
+    List<Alumno> findBySeccion(String seccion);
 }
