c8dc0c9b4f9b1923c22d0356c336e57ba042e5fb
alisson <alissonfranco56@gmail.com>
2026-07-15 09:06:20 -0500

    database

--- sistema_notas.sql
+++ sistema_notas.sql
@@ -32,16 +32,6 @@ CREATE TABLE `administrador` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `administrador`
---
-
-LOCK TABLES `administrador` WRITE;
-/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
-INSERT INTO `administrador` VALUES (1,1);
-/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `alumno`
 --
 
@@ -61,18 +51,8 @@ CREATE TABLE `alumno` (
   UNIQUE KEY `UK8niai0o699f9bterqc7obpwyp` (`id_usuario`),
   UNIQUE KEY `UKtgfx9d7amj17rj56egq208pgl` (`codigo_institucional`),
   CONSTRAINT `FK3j6mgfljwrkb96hnya4lc1fhd` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `alumno`
---
-
-LOCK TABLES `alumno` WRITE;
-/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
-INSERT INTO `alumno` VALUES (_binary '','2011-06-09',1,5,'Rivas Sanchez','AL1222','Mario James','A');
-/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `asistencia`
@@ -93,18 +73,8 @@ CREATE TABLE `asistencia` (
   KEY `FKiboik92fgov8skdrwk760eqee` (`id_curso`),
   CONSTRAINT `FKhoistiucdphdkhv4gcsan5o0d` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
   CONSTRAINT `FKiboik92fgov8skdrwk760eqee` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20741 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `asistencia`
---
-
-LOCK TABLES `asistencia` WRITE;
-/*!40000 ALTER TABLE `asistencia` DISABLE KEYS */;
-INSERT INTO `asistencia` VALUES ('2026-06-09',1,1,5,NULL,'TARDANZA'),('2026-06-07',1,2,5,NULL,'ASISTIO'),('2026-06-01',1,3,5,NULL,'FALTA');
-/*!40000 ALTER TABLE `asistencia` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `curso`
@@ -127,16 +97,6 @@ CREATE TABLE `curso` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `curso`
---
-
-LOCK TABLES `curso` WRITE;
-/*!40000 ALTER TABLE `curso` DISABLE KEYS */;
-INSERT INTO `curso` VALUES (_binary '',1,2,'Álgebra, Geometría, Aritmética y Trigonometría','Matemática','A'),(_binary '',2,NULL,'Literatura, Gramática y Razonamiento Verbal','Comunicación','A'),(_binary '',3,NULL,'Idioma extranjero','Inglés','A'),(_binary '',4,NULL,'Expresión artística, danza y artes visuales','Arte y Cultura','A'),(_binary '',5,1,'Historia del Perú y del Mundo, Geografía y Economía','Ciencias Sociales','A'),(_binary '',6,NULL,'Ciudadanía, Cívica y Psicología','Desarrollo Personal (DPCC)','A'),(_binary '',7,NULL,'Deportes, psicomotricidad y vida saludable','Educación Física','A'),(_binary '',8,NULL,'Formación en valores y religión','Educación Religiosa','A'),(_binary '',9,3,'Física, Química y Biología','Ciencia y Tecnología','A'),(_binary '',10,NULL,'Emprendimiento y gestión de proyectos','Educación para el Trabajo (EPT)','A'),(_binary '',11,NULL,'Acompañamiento socioafectivo y cognitivo','Tutoría y Orientación','A'),(_binary '',12,NULL,'Herramientas TIC y ofimática','Computación e Informática','A'),(_binary '',13,NULL,'Álgebra, Geometría, Aritmética y Trigonometría','Matemática','B'),(_binary '',14,NULL,'Literatura, Gramática y Razonamiento Verbal','Comunicación','B'),(_binary '',15,NULL,'Idioma extranjero','Inglés','B'),(_binary '',16,NULL,'Expresión artística, danza y artes visuales','Arte y Cultura','B'),(_binary '',17,NULL,'Historia del Perú y del Mundo, Geografía y Economía','Ciencias Sociales','B'),(_binary '',18,NULL,'Ciudadanía, Cívica y Psicología','Desarrollo Personal (DPCC)','B'),(_binary '',19,NULL,'Deportes, psicomotricidad y vida saludable','Educación Física','B'),(_binary '',20,NULL,'Formación en valores y religión','Educación Religiosa','B'),(_binary '',21,NULL,'Física, Química y Biología','Ciencia y Tecnología','B'),(_binary '',22,NULL,'Emprendimiento y gestión de proyectos','Educación para el Trabajo (EPT)','B'),(_binary '',23,NULL,'Acompañamiento socioafectivo y cognitivo','Tutoría y Orientación','B'),(_binary '',24,2,'Herramientas TIC y ofimática','Computación e Informática','B'),(_binary '',25,NULL,'Álgebra, Geometría, Aritmética y Trigonometría','Matemática','C'),(_binary '',26,NULL,'Literatura, Gramática y Razonamiento Verbal','Comunicación','C'),(_binary '',27,NULL,'Idioma extranjero','Inglés','C'),(_binary '',28,NULL,'Expresión artística, danza y artes visuales','Arte y Cultura','C'),(_binary '',29,NULL,'Historia del Perú y del Mundo, Geografía y Economía','Ciencias Sociales','C'),(_binary '',30,1,'Ciudadanía, Cívica y Psicología','Desarrollo Personal (DPCC)','C'),(_binary '',31,NULL,'Deportes, psicomotricidad y vida saludable','Educación Física','C'),(_binary '',32,NULL,'Formación en valores y religión','Educación Religiosa','C'),(_binary '',33,NULL,'Física, Química y Biología','Ciencia y Tecnología','C'),(_binary '',34,3,'Emprendimiento y gestión de proyectos','Educación para el Trabajo (EPT)','C'),(_binary '',35,NULL,'Acompañamiento socioafectivo y cognitivo','Tutoría y Orientación','C'),(_binary '',36,NULL,'Herramientas TIC y ofimática','Computación e Informática','C');
-/*!40000 ALTER TABLE `curso` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `docente`
 --
 
@@ -153,18 +113,26 @@ CREATE TABLE `docente` (
   UNIQUE KEY `UKg59bvniliwyev7wy00glxe17a` (`id_usuario`),
   UNIQUE KEY `UKrc3jw3kr1j23xwa7ylg397smd` (`codigo_institucional`),
   CONSTRAINT `FK86smr8ieg6n2thvd52c336w9n` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `docente`
+-- Table structure for table `encuesta_docente`
 --
 
-LOCK TABLES `docente` WRITE;
-/*!40000 ALTER TABLE `docente` DISABLE KEYS */;
-INSERT INTO `docente` VALUES (_binary '\0',1,2,'DOC-12345678',NULL),(_binary '\0',2,3,'DOC-37821234',NULL),(_binary '\0',3,4,'DOC-123555',NULL);
-/*!40000 ALTER TABLE `docente` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `encuesta_docente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `encuesta_docente` (
+  `id_encuesta` int NOT NULL AUTO_INCREMENT,
+  `calificacion` int NOT NULL,
+  `fecha_respuesta` date NOT NULL,
+  `id_docente` int NOT NULL,
+  PRIMARY KEY (`id_encuesta`),
+  KEY `FKacjetfa8diq8j82o96032db5d` (`id_docente`),
+  CONSTRAINT `FKacjetfa8diq8j82o96032db5d` FOREIGN KEY (`id_docente`) REFERENCES `docente` (`id_docente`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `evaluacion`
@@ -184,18 +152,8 @@ CREATE TABLE `evaluacion` (
   PRIMARY KEY (`id_evaluacion`),
   KEY `FKk4l3846ghken3y7kc09dses65` (`id_curso`),
   CONSTRAINT `FKk4l3846ghken3y7kc09dses65` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=146 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `evaluacion`
---
-
-LOCK TABLES `evaluacion` WRITE;
-/*!40000 ALTER TABLE `evaluacion` DISABLE KEYS */;
-INSERT INTO `evaluacion` VALUES (_binary '','2026-06-07',5,1,15.00,'PC1','PRACTICA');
-/*!40000 ALTER TABLE `evaluacion` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `matricula`
@@ -219,15 +177,6 @@ CREATE TABLE `matricula` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `matricula`
---
-
-LOCK TABLES `matricula` WRITE;
-/*!40000 ALTER TABLE `matricula` DISABLE KEYS */;
-/*!40000 ALTER TABLE `matricula` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `nota`
 --
 
@@ -241,23 +190,14 @@ CREATE TABLE `nota` (
   `id_nota` int NOT NULL AUTO_INCREMENT,
   `valor` decimal(4,2) NOT NULL,
   `observacion` varchar(255) DEFAULT NULL,
+  `corregida` bit(1) NOT NULL,
   PRIMARY KEY (`id_nota`),
   KEY `FKer89uet8f033o2npm6j7e46xh` (`id_alumno`),
   KEY `FKtiqarr5h1nns2pyj428uvr669` (`id_evaluacion`),
   CONSTRAINT `FKer89uet8f033o2npm6j7e46xh` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
   CONSTRAINT `FKtiqarr5h1nns2pyj428uvr669` FOREIGN KEY (`id_evaluacion`) REFERENCES `evaluacion` (`id_evaluacion`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4097 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `nota`
---
-
-LOCK TABLES `nota` WRITE;
-/*!40000 ALTER TABLE `nota` DISABLE KEYS */;
-INSERT INTO `nota` VALUES ('2026-06-09',1,1,1,12.00,NULL);
-/*!40000 ALTER TABLE `nota` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `usuario`
@@ -276,18 +216,8 @@ CREATE TABLE `usuario` (
   `rol` enum('ADMIN','ALUMNO','DOCENTE') NOT NULL,
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `UK2mlfr087gb1ce55f2j87o74t` (`correo`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (_binary '',1,'Principal','$2a$10$0YrJl6FntLL6bjOA35HVG.Ge8WSE7HiID5.vWfCtu0q/tUki.WYei','admin@jcm.edu.pe','Admin','ADMIN'),(_binary '',2,'Gamarra Lopez','$2a$10$ZFHg1ItUG3AO4R/T.PbyDuJrMrZDgDPi8b2uIWxBv113d50tPcgfe','mgamarral@jcm.edu.pe','Maria Gracia','DOCENTE'),(_binary '',3,'Espinoza Cueva','$2a$10$ad1GGHtgdgjFnO5wcc8.UOayGooC3352YHustDay1/gu4YpqCbkJm','respinozac@jcm.edu.pe','Rosa Maria','DOCENTE'),(_binary '',4,'Maartinz Lopez','$2a$10$oKURDoRrJ0xyHkUsraPyo.X1RXLJcaLqriOedislKyOjmszFvrHQO','jmartinezl@jcm.edu.pe','Jose Luis','DOCENTE'),(_binary '',5,'Rivas Sanchez','$2a$10$9/Mmbf0zQkXSyP.SBTMI1u.V3csZxUIt.41Skxvc3jJiGzYZNQFhW','mrivass@jcm.edu.pe','Mario James','ALUMNO');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -298,4 +228,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-10  8:51:39
+-- Dump completed on 2026-07-15  8:28:27
